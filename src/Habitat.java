d281e8f4abfac60a5b540fb10116bd3d4e92bc2d
Chad Clough <chadnathanclough@gmail.com>
2015-06-22 21:42:09 -0400

    created a main method with easy to read results and added default values for generations and populationSize

--- src/Habitat.java
+++ src/Habitat.java
@@ -5,25 +5,23 @@ import org.jgap.impl.*;
  * @since 6/21/2015
  */
 public class Habitat {
-	private int generations;
-	private int populationSize;
+	private int generations = 400;
+	private int populationSize = 100;
 	private Configuration conf;
 	private Genotype population;
-	private Item[] inventory;
+	private static Item[] inventory;
 	private Knapsack sack;
-	
-
 	
 	@SuppressWarnings("unused")
 	private Habitat(){
 		//restrict default constructor
 	}
 	
-	public Habitat(Item[] inv){
+	public Habitat(Item[] inv, double maxWeight){
 		try{
 			conf = new DefaultConfiguration();
 			inventory = inv;
-			sack = new Knapsack(inventory.length);
+			sack = new Knapsack(maxWeight);
 			
 			conf.setSampleChromosome(new Chromosome(conf,createSampleGenes()));
 			conf.setFitnessFunction(new KnapsackFitnessFunction());
@@ -39,8 +37,8 @@ public class Habitat {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * @return sample genes for a knapsack chromosome. Used for setting a
+	 * 	sample chromosome for a jgap Configuration object
 	 */
 	private Gene[] createSampleGenes(){
 		try{
@@ -77,8 +75,63 @@ public class Habitat {
 		}		
 	}
 	
-	public static void main(String[] args){
+	/**
+	 * Creates a string that is an easy to read version of a solution.
+	 * 
+	 * Example Output
+	 * --------------------------
+	 * Solution:
+	 * value = 32.0	weight = 7.0
+	 *
+	 * Item	Value	Weight	Density
+	 * 2	20.0	1.0		20.0
+	 * 4	9.0		5.0		1.8
+	 * 5	3.0		1.0		3.0
+	 * 
+	 * @param solution The solution to prettify
+	 * @return A string of a solution that provides information on the items
+	 * 	in the solution as well as making the solution easy to read
+	 */
+	public static String prettifySolution(IChromosome solution){
+		Gene[] genes = solution.getGenes();
+		String ret = "Solution:\nvalue = ";
+		String items = "\n\nItem\tValue\tWeight\tDensity";
 		
+		double value = 0, weight = 0;
+		Item item;
+		for(int i = 1; i < genes.length; i++){
+			item = inventory[i-1];
+			if((Boolean)genes[i].getAllele()){
+				items += "\n" + (i-1) + "\t" + item.toString();
+				value += item.getValue();
+				weight += item.getWeight();
+			}
+		}
+		
+		ret += "" + value + "\tweight = " + weight + items;
+		
+		return ret;
+	}
+	
+	public static Item[] getInventory(){
+		return inventory;
+	}
+	
+	public static void main(String[] args){
+		//display problem
+		System.out.println("This knapsack can hold 10 lbs.");
+		System.out.println("Here's the list of items:\n"
+				+ "Value\tWeight\tDensity");
+		String items = "3:5;1:20;20:1;2:4;9:5;3:1;15:10";
+		Item[] inv = Item.parseItems(items);
+		for(Item i : inv) System.out.println(i);
+		
+		//calculate solution
+		System.out.println("\nCreating a new habitat...");
+		Habitat habitat = new Habitat(inv, 10);
+		System.out.println("Getting a solution...\n");
+		IChromosome solution = habitat.run();
+		System.out.println(prettifySolution(solution));
 	}
 
 }
