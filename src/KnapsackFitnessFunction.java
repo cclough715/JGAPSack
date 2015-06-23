4ac86c669be6c04e4e82695f552e0881e3b80925
Chad Clough <chadnathanclough@gmail.com>
2015-06-22 21:52:40 -0400

    Implemented the FitnessFunction

--- src/KnapsackFitnessFunction.java
+++ src/KnapsackFitnessFunction.java
@@ -1,4 +1,5 @@
 import org.jgap.FitnessFunction;
+import org.jgap.Gene;
 import org.jgap.IChromosome;
 /**
  * @author Chad Clough
@@ -9,11 +10,22 @@ public class KnapsackFitnessFunction extends FitnessFunction {
 
 	@Override
 	protected double evaluate(IChromosome arg0) {
-		// TODO Auto-generated method stub
-		
-		
-		
-		return 0;
+		try{
+			Gene[] genes = arg0.getGenes();
+			double maxWeight = (Double)genes[0].getAllele();
+			Knapsack sack = new Knapsack(maxWeight);
+			
+			for(int i = 1; i < genes.length; i++)
+				if((Boolean)genes[i].getAllele())
+					//if an item cannot be added then this solution is invalid
+					if(!sack.addItem(Habitat.getInventory()[i-1]))
+						return 0;
+				
+			return sack.getCurrentValue();
+		}catch(Exception e){
+			e.printStackTrace();
+			return 0;
+		}
 	}
 
 }
